8c93d82d8a90d367a1aad6ba10ffc50b0317255a
akshayakky <akshaysmpt01@gmail.com>
2020-02-19 12:54:53 +0530

    ADDED : Win And Lose Feature And Store Remaining Money

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -2,5 +2,21 @@
 
 echo  Welcome To Gambling Simulation Problem
 
+#CONSTANTS
 STAKE_PER_DAY=100
 BET_PER_GAME=1
+WIN=1
+
+#VARIABLES
+money=$STAKE_PER_DAY
+checkWin=$((RANDOM%2))
+
+#CHECKING FOR WIN OR LOSE AND CALCULATE MONEY
+if [ $checkWin -eq $WIN ]
+then
+	echo Gambler Won
+	money=$(($money+$BET_PER_GAME))
+else
+	echo Gambler Lost
+	money=$(($money-$BET_PER_GAME))
+fi
