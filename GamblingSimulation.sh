d940568a7dffbcfa909ce867e9c195cf1b0e9f20
akshayakky <akshaysmpt01@gmail.com>
2020-02-20 09:31:40 +0530

    ADDED : Added Luckiest And Unluckiest Day Feature

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -12,6 +12,8 @@ MIN_LIMIT=$(($STAKE_PER_DAY-$STAKE_PER_DAY*$GOAL_PERCENT/100))
 
 #VARIABLES
 moneyWonOrLostInDay=50
+daysLost=0
+daysWon=0
 
 #FUNCTION TO SIMULATE GAMBLING FOR A DAY
 function getMoneyWonOrLostForDay(){
@@ -30,20 +32,55 @@ function getMoneyWonOrLostForDay(){
 	echo $moneyAtEndOfDay
 }
 
-#FOR LOOP TO SIMULATE GAMBLING FOR A MONTH
-for (( i=1; i<=30; i++ ))
-do
-	moneyWonOrLostForDay=$(getMoneyWonOrLostForDay)
-	if [ $moneyWonOrLostForDay -lt 0 ]
-	then
-		#STORING NO OF DAYS LOST AND NO OF DAYS WON
-		((daysLost++))
-	else
-		((daysWon++))
-	fi
-	totalMoney=$(($totalMoney+$moneyWonOrLostForDay))
-	dayAndMoneyDictionary[$i]=$totalMoney
-done
+#FUNCTION TO SIMULATE GAMBLING FOR A MONTH AND STORE RESULTS IN ARRAY
+function getDayAndMoneyWonArrayForMonth(){
+	#FOR LOOP TO SIMULATE GAMBLING FOR A MONTH
+	for (( i=1; i<=30; i++ ))
+	do
+		moneyWonOrLostForDay=$(getMoneyWonOrLostForDay)
+		if [ $moneyWonOrLostForDay -lt 0 ]
+		then
+			#STORING NO OF DAYS LOST AND NO OF DAYS WON
+			((daysLost++))
+		else
+			((daysWon++))
+		fi
+		totalMoney=$(($totalMoney+$moneyWonOrLostForDay))
+		dayAndMoneyWonArray[$i]=$totalMoney
+	done
+}
+
+#FUNCTION TO FIND LUCKIEST AND UNLUCKIEST DAY FOR GAMBLER
+function getLuckiestDayAndUnluckiestDay(){
+	local maximumLostAmount=$((2**63-1))
+	local maximumWonAmount=$((-1*$((2**63-1))))
+	for (( i=1; i<=$((${#dayAndMoneyWonArray[@]})); i++ ))
+	do
+		if [ ${dayAndMoneyWonArray[i]} -gt $maximumWonAmount ]
+		then
+			luckiestDay=""
+			maximumWonAmount=${dayAndMoneyWonArray[i]}
+			luckiestDay=$i
+		elif [ ${dayAndMoneyWonArray[i]} -eq $maximumWonAmount ]
+		then
+			luckiestDay=$luckiestDay" "$i
+		fi
+
+		if [ ${dayAndMoneyWonArray[i]} -lt $maximumLostAmount ]
+		then
+			unLuckiestDay=""
+			maximumLostAmount=${dayAndMoneyWonArray[i]}
+			unLuckiestDay=$i
+		elif [ ${dayAndMoneyWonArray[i]} -eq $maximumLostAmount ]
+		then
+			unLuckiestDay=$unLuckiestDay" "$i
+		fi
+	done
+}
+
+
+getDayAndMoneyWonArrayForMonth
+getLuckiestDayAndUnluckiestDay
 
 #FINDING MONEY LOST AND WON IN MONTH
 moneyLostInMonth=$(($daysLost*$moneyWonOrLostInDay))
